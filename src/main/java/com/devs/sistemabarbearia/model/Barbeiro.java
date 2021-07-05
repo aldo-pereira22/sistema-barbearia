63b184116d2b46764c42fb357a39406152c1d803
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-05 10:53:02 -0300

    Recurso de servicos

--- src/main/java/com/devs/sistemabarbearia/model/Barbeiro.java
+++ src/main/java/com/devs/sistemabarbearia/model/Barbeiro.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -30,7 +29,7 @@ public class Barbeiro {
 	List<Servico> listaServicos = new ArrayList<Servico>();
 	
 	public Barbeiro() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public Barbeiro(Long id, String cpf, String nome, String email, String senha, String tipo) {
---
+++ src/main/java/com/devs/sistemabarbearia/pessoa/repository/ServicoRepository.java
@@ -0,0 +1,10 @@
+package com.devs.sistemabarbearia.pessoa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devs.sistemabarbearia.model.Servico;
+
+public interface ServicoRepository extends JpaRepository<Servico, Long>{
+
+	
+}
